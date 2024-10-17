96b86ddb334772f6a5dcbcc8dccb413581fb1bd4
hlaolil <tlokza@gmail.com@>
2024-10-17 22:40:58 +0200

    w5

---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// Get the current year for footer
+const date = new Date();
+year.textContent = date.getFullYear();
+
+// Get the last modification date of the home page
+let lastMod = document.lastModified;
+lastModified.textContent = `Last Modified: ${lastMod}`;

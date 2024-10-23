110c86ad3ced3af2a7ce7e63c6bec85a5bd7d287
hlaolil <167699704+hlaolil@users.noreply.github.com>
2024-10-23 22:41:17 +0200

    Update grace.js

--- project/scripts/grace.js
+++ project/scripts/grace.js
@@ -1,1 +1,10 @@
+// Select hamburger and navigation menu
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navigation');
 
+// Toggle 'open' class on the navigation when hamburger is clicked
+hamburger.addEventListener('click', () => {
+    const expanded = hamburger.getAttribute('aria-expanded') === 'true' || false;
+    hamburger.setAttribute('aria-expanded', !expanded);
+    navMenu.classList.toggle('open');
+});
